1604ad269b425179a6fd00f1167d447dd5836b7e
JebacKoda <55604333+JebacKoda@users.noreply.github.com>
2020-06-10 10:45:35 +0200

    e

---
+++ js/pretext.js
@@ -0,0 +1,83 @@
+function reply_click(clicked_id)
+{
+    if(clicked_id === "5035")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o bagr Kramer 5035';
+        document.getElementById('form32').value = 'Kramer 5035';
+    }
+    if(clicked_id === "5065")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o bagr Kramer 5065';
+        document.getElementById('form32').value = 'Kramer 5065';
+    }
+    if(clicked_id === "1245")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o manipulátor Kramer 1245';
+        document.getElementById('form32').value = 'Kramer 1245';
+    }
+    if(clicked_id === "2706")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o manipulátor Kramer 2706';
+        document.getElementById('form32').value = 'Kramer 2706';
+    }
+    if(clicked_id === "3507")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o manipulátor Kramer 3507';
+        document.getElementById('form32').value = 'Kramer 3507';
+    }
+    if(clicked_id === "4407")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o manipulátor Kramer 4407';
+        document.getElementById('form32').value = 'Kramer 4407';
+    }
+    if(clicked_id === "5035")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5035';
+        document.getElementById('form32').value = 'Kramer 5035';
+    }
+    if(clicked_id === "5055")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5055';
+        document.getElementById('form32').value = 'Kramer 5055';
+    }
+    if(clicked_id === "5065")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5065';
+        document.getElementById('form32').value = 'Kramer 5065';
+    }
+    if(clicked_id === "5065t")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5065t';
+        document.getElementById('form32').value = 'Kramer 5065T';
+    }
+    if(clicked_id === "5075")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5075';
+        document.getElementById('form32').value = 'Kramer 5075';
+    }
+    if(clicked_id === "5085")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 5085';
+        document.getElementById('form32').value = 'Kramer 5085';
+    }
+    if(clicked_id === "8085")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 8085';
+        document.getElementById('form32').value = 'Kramer 8085';
+    }
+    if(clicked_id === "8085T")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 8085T';
+        document.getElementById('form32').value = 'Kramer 8085T';
+    }
+    if(clicked_id === "8105")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 8105';
+        document.getElementById('form32').value = 'Kramer 8105';
+    }
+    if(clicked_id === "8115")
+    {
+        document.getElementById('form5035').value = 'Dobrý den,\nmám zájem o nakladač Kramer 8115';
+        document.getElementById('form32').value = 'Kramer 8115';
+    }
+}
